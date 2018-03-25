c2bb51b0574888d1c7c551b24d2213b14208a4e1
doujianbin <23931216@qq.com>
2018-03-25 20:00:49 +0800

    商品暂存

---
+++ GouKuSeller/Controllers/商品/CommodityStatusView.h
@@ -0,0 +1,13 @@
+//
+//  CommodityStatusView.h
+//  GouKuSeller
+//
+//  Created by 窦建斌 on 2018/3/22.
+//  Copyright © 2018年 窦建斌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommodityStatusView : UIView
+
+@end
---
+++ GouKuSeller/Controllers/商品/CommodityStatusView.m
@@ -0,0 +1,21 @@
+//
+//  CommodityStatusView.m
+//  GouKuSeller
+//
+//  Created by 窦建斌 on 2018/3/22.
+//  Copyright © 2018年 窦建斌. All rights reserved.
+//
+
+#import "CommodityStatusView.h"
+
+@implementation CommodityStatusView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
