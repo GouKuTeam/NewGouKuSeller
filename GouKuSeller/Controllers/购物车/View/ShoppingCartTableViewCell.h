bdff8408c45dce00b07d47ab3350dcab36de14ac
xiao817287 <li817287@gmail.com>
2018-05-03 16:39:55 +0800

    update---购物车UI

---
+++ GouKuSeller/Controllers/购物车/View/ShoppingCartTableViewCell.h
@@ -0,0 +1,22 @@
+//
+//  ShoppingCartTableViewCell.h
+//  GouKuSeller
+//
+//  Created by lixiao on 2018/5/3.
+//  Copyright © 2018年 窦建斌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShoppingCartTableViewCell : UITableViewCell
+
+@property (nonatomic,strong)UIButton      *btn_select;
+@property (nonatomic,strong)UIImageView   *iv_image;
+@property (nonatomic,strong)UILabel       *lb_name;
+@property (nonatomic,strong)UILabel       *lb_price;
+@property (nonatomic,strong)UILabel       *lb_specification;
+@property (nonatomic,strong)UIButton      *btn_less;
+@property (nonatomic,strong)UIButton      *btn_plus;
+@property (nonatomic,strong)UITextField   *tf_number;
+
+@end
