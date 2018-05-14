ac5bd97d9cf6628d1d61dea8cf2311493fa83fbb
xiao817287 <li817287@gmail.com>
2018-05-14 13:37:21 +0800

    update

---
+++ GouKuSeller/Controllers/商家端/New Group1/采购/供应商/View/SelectUnitView.h
@@ -0,0 +1,16 @@
+//
+//  SelectUnitView.h
+//  GouKuSeller
+//
+//  Created by lixiao on 2018/5/14.
+//  Copyright © 2018年 窦建斌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SelectUnitView : UIView
+
+@property(nonatomic,strong)UIButton               *btn_add;
+@property(nonatomic,strong)UICollectionView       *collectionView;
+
+@end
---
+++ GouKuSeller/Controllers/商家端/New Group1/采购/供应商/View/SelectUnitView.m
@@ -0,0 +1,22 @@
+//
+//  SelectUnitView.m
+//  GouKuSeller
+//
+//  Created by lixiao on 2018/5/14.
+//  Copyright © 2018年 窦建斌. All rights reserved.
+//
+
+#import "SelectUnitView.h"
+
+@implementation SelectUnitView
+
+- (instancetype)initWithFrame:(CGRect)frame{
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+        
+    }
+    return self;
+}
+
+@end
