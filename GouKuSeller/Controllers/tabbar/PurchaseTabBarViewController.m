5d92663f555d3f4dc5ee72821af9c55e03fa7f8d
xiao817287 <li817287@gmail.com>
2018-05-21 15:47:43 +0800

    update

--- GouKuSeller/Controllers/tabbar/PurchaseTabBarViewController.m
+++ GouKuSeller/Controllers/tabbar/PurchaseTabBarViewController.m
@@ -111,10 +111,12 @@
     badgeView.layer.cornerRadius = 8;
     badgeView.layer.masksToBounds = YES;
     badgeView.text = str_count;
-    badgeView.textColor = [UIColor whiteColor];
+    badgeView.textColor = [UIColor colorWithHexString:COLOR_BLUE_MAIN];
     badgeView.font = [UIFont systemFontOfSize:FONT_SIZE_MEMO];
     badgeView.textAlignment = NSTextAlignmentCenter;
-    badgeView.backgroundColor = [UIColor colorWithHexString:@"#FF3B30"];
+    badgeView.backgroundColor = [UIColor whiteColor];
+    badgeView.layer.borderColor = [[UIColor colorWithHexString:COLOR_BLUE_MAIN] CGColor];
+    badgeView.layer.borderWidth = 0.5;
     CGRect tabFrame = self.tabBar.frame;
     
     //确定小红点的位置
