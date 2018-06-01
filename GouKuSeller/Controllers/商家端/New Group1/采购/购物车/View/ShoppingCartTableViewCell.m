2fd9606974032d334e4238d80bcfc7f3838890c1
xiao817287 <li817287@gmail.com>
2018-06-01 14:24:19 +0800

    update--UI

--- GouKuSeller/Controllers/商家端/New Group1/采购/购物车/View/ShoppingCartTableViewCell.m
+++ GouKuSeller/Controllers/商家端/New Group1/采购/购物车/View/ShoppingCartTableViewCell.m
@@ -74,6 +74,7 @@
             make.left.equalTo(self.iv_image.mas_right).offset(10);
             make.top.equalTo(self.iv_image);
             make.right.equalTo(self.mas_right).offset(-10);
+            make.height.mas_lessThanOrEqualTo(32);
         }];
         
         [self.lb_price mas_makeConstraints:^(MASConstraintMaker *make) {
