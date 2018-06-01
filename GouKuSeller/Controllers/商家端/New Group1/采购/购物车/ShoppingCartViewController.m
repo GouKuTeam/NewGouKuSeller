020989cb6d48915eace9d8b47184109618784826
xiao817287 <li817287@gmail.com>
2018-06-01 14:17:43 +0800

    update

--- GouKuSeller/Controllers/商家端/New Group1/采购/购物车/ShoppingCartViewController.m
+++ GouKuSeller/Controllers/商家端/New Group1/采购/购物车/ShoppingCartViewController.m
@@ -508,7 +508,7 @@
         NSMutableArray *arr_selectResult = [NSMutableArray array];
         NSMutableArray *arr_unSelect = [NSMutableArray array];
         for (int i = 0; i < self.arr_select.count; i++) {
-            StoreEntity *storeEntity = [[self.arr_select objectAtIndex:i] copy];
+            StoreEntity *storeEntity = [self.arr_select objectAtIndex:i];
             double sectionAmount = 0.00;
             for (SupplierCommodityEndity *entity in storeEntity.shoppingCatItems) {
                 sectionAmount = sectionAmount + entity.count * entity.price;
@@ -527,7 +527,7 @@
                 [MBProgressHUD showErrorMessage:[NSString stringWithFormat:@"在%@等供应商的购买金额没有达到起送价",storeEntity.name]];
             }
             for (int i = 0; i < arr_unSelect.count; i++) {
-                StoreEntity *storeEntity = [[self.arr_select objectAtIndex:i] copy];
+                StoreEntity *storeEntity = [arr_unSelect objectAtIndex:i];
                 if ([self.arr_select containsObject:storeEntity]) {
                     storeEntity.shoppingCatItems = @[];
                     storeEntity.isSelected = NO;
